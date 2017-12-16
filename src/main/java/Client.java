966f9582dcc93710bf26e87ff5bae67f53d38acf
Vinge1718 <vinge1718@gmail.com>
2017-12-16 15:50:29 +0300

    add the logic to locate indivudual clients

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -5,14 +5,45 @@ import org.sql2o.*;
 
 
 public class Client{
+    private int mId;
     private String mName;
+    private boolean mCompleted;
+    private LocalDateTime mCreatedAt;
+    private static List<Client> instances = new ArrayList<Client>();
 
     public Client(String name) {
         mName = name;
-  }
+        mCompleted = false;
+        mCreatedAt = LocalDateTime.now();
+        instances.add(this);
+        mId = instances.size();
+    }
 
     public String getName() {
         return mName;
-  }
+    }
 
+    public boolean isCompleted() {
+        return mCompleted;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return mCreatedAt;
+    }
+
+    public static List<Client> all() {
+        return instances;
+    }
+
+    public static void clear() {
+        instances.clear();
+    }
+
+    public int getId() {
+        return mId;
+    }
+
+    public static Client find(int id) {
+        return instances.get(id - 1);
+    }
 }
