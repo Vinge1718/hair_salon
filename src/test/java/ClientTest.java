6a020c9e8c3e26a8dec07f00d9d1059638c78935
Vinge1718 <vinge1718@gmail.com>
2017-12-16 15:57:06 +0300

    add the logic to test the apps ability to display individual clients

--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -18,4 +18,45 @@ public class ClientTest{
         Client myClient = new Client("Mary Jane");
         assertEquals("Mary Jane", myClient.getName());
     }
+
+    @Test
+    public void isCompleted_isFalseAfterInstantiation_false() {
+        Client myClient = new Client("Mary Jane");
+        assertEquals(false, myClient.isCompleted());
+    }
+
+    @Test
+    public void getCreatedAt_instantiatesWithCurrentTime_today() {
+        Client myClient = new Client("Mary Jane");
+        assertEquals(LocalDateTime.now().getDayOfWeek(), myClient.getCreatedAt().getDayOfWeek());
+    }
+
+    @Test
+    public void all_returnsAllInstancesOfClient_true() {
+        Client firstClient = new Client("Peterson Joy");
+        Client secondClient = new Client("Kylie Kate");
+        assertEquals(true, Client.all().contains(firstClient));
+        assertEquals(true, Client.all().contains(secondClient));
+    }
+
+    @Test
+    public void clear_emptiesAllClientsFromArrayList_0() {
+        Client myClient = new Client("Mary Jane");
+        Client.clear();
+        assertEquals(Client.all().size(), 0);
+    }
+
+    @Test
+    public void getId_clientsInstantiateWithAnID_1() {
+        Client.clear();  // Remember, the test will fail without this line! We need to empty leftover Tasks from previous tests!
+        Client myClient = new Client("Morrison Grymes");
+        assertEquals(1, myClient.getId());
+    }
+
+    @Test
+    public void find_returnsClientWithSameId_secondClient() {
+        Client firstClient = new Client("Peterson Joy");
+        Client secondClient = new Client("Kylie Kate");
+        assertEquals(Client.find(secondClient.getId()), secondClient);
+    }
 }
